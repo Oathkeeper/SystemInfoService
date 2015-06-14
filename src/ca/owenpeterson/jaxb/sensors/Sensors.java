278479cbff4dc02fa5aa487c5378d2c7d99daaf6
opeterson <owen.a.peterson@gmail.com>
2015-06-14 11:42:07 -0500

    added requestDate element to sensors output. Fixed logging location in local.properties

--- src/ca/owenpeterson/jaxb/sensors/Sensors.java
+++ src/ca/owenpeterson/jaxb/sensors/Sensors.java
@@ -1,11 +1,10 @@
 package ca.owenpeterson.jaxb.sensors;
 
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.Date;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
@@ -14,14 +13,21 @@ public class Sensors implements Serializable {
 
 	private static final long serialVersionUID = -2313158679550959653L;
 	
+	private Date requestDate;
 	private AdapterList adapters;
-	
+		
 	public Sensors(AdapterList adapters) {
 		this.adapters = adapters;
+		this.requestDate = new Date();
 	}
 	
 	public Sensors() {
-		this.adapters = new AdapterList();		
+		this.adapters = new AdapterList();
+		this.requestDate = new Date();
+	}
+	
+	public Date getRequestDate() {
+		return requestDate;
 	}
 
 	public AdapterList getAdapters() {
@@ -31,5 +37,11 @@ public class Sensors implements Serializable {
 	public void setAdapters(AdapterList adapters) {
 		this.adapters = adapters;
 	}
+
+	public void setRequestDate(Date requestDate) {
+		this.requestDate = requestDate;
+	}
+	
+	
 
 }
